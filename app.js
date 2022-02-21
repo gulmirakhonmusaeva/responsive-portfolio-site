ee2f342b9c26410f36d52a52b55405f44ccc6b45
gulmirakhonmusaeva <gulmirakhonmusaeva@gmail.com>
2022-02-21 07:17:45 +0500

    initial git

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+
+let typed = new Typed('#words', {
+   
+    strings: ['Blogger', 'Web Developer','YouTuber'],
+    typeSpeed: 100,
+    startDelay: 1000,
+    backSpeed: 100,
+    smartBackspace: true,
+    loop: true,
+  });
